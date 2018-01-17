d2b481b4e0a29607f6bf5b355f04a12416750516
zewenxu <zewenxu@fintend.com>
2018-01-17 18:27:08 +0800

    libfsc

--- Debug/stmp/subdir.mk
+++ Debug/stmp/subdir.mk
@@ -44,7 +44,7 @@ CPP_DEPS += \
 stmp/%.o: ../stmp/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -DLIBFSC -DLINUX -I"/root/c/project/ethex/libmisc-c" -I"/root/c/project/dev/libsrv-comm-c" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -DLIBFSC -DLINUX -I"/root/c/project/dev/libmisc-c" -I"/root/c/project/dev/libsrv-comm-c" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
